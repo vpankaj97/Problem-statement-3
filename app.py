1901a28b5259e7400cfb3157e6db56497a984101
Roshan Kumar <roshankumarxc@gmail.com>
2024-12-19 19:51:55 +0530

    update 2

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import joblib
 import numpy as np
 import pickle
 
-# Load the trained model
+
 model = joblib.load(r'fraud_detection_model.pkl')
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get JSON data from request
+    
     data = request.json
     input_data = np.array([data['features']])
     prediction = model.predict(input_data)[0]
--- fraud_detection.py
+++ fraud_detection.py
@@ -5,24 +5,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 import joblib
 
-# Load sample data
-# You can replace this with an actual dataset or a publicly available dataset
+
 url = r"C:\Users\rosha\Downloads\insuranceFraud_Dataset.csv"
 data = pd.read_csv(url)
 print(data)
-# Display data overview
+
 print("Data Overview:")
 print(data.head())
 
-# Preprocessing: Drop unnecessary columns
+
 data = data.drop(columns=['policy_number', 'insured_zip', 'incident_location'])
 
-# Encode categorical variables
+
 data = pd.get_dummies(data, drop_first=True)
 
-# Split into features (X) and target (y)
+
 X = data.drop(columns=['fraud_reported_Y'])
 y = data['fraud_reported_Y']
 
-# Train-test split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
